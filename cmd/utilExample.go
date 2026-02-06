eb640d31155d3ac4e29a2b421b766c5f398695a5
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-06 16:46:58 +0200

    update example

--- cmd/utilExample.go
+++ cmd/utilExample.go
@@ -12,27 +12,46 @@ import (
 
 var exampleRoot = domovoi.FormatExample(
 	"hypnos",
-	[]string{"help"},
+	[]string{
+		"hypnos awaken",
+		"hypnos hibernate --probe pmail --script \"open -a Mail\" --duration 5m",
+		"hypnos scan",
+		"hypnos stasis pmail",
+	},
 )
 
 var exampleAwaken = domovoi.FormatExample(
-	"awaken",
-	[]string{"help"},
+	"hypnos awaken",
+	[]string{
+		"hypnos awaken",
+		"hypnos awaken --config-output ~/.hypnos/config/example.toml",
+	},
 )
 
 var exampleHibernate = domovoi.FormatExample(
-	"hypnos",
-	[]string{"help"},
+	"hypnos hibernate",
+	[]string{
+		"hypnos hibernate --probe focus --script \"say 'Done'\" --duration 25m",
+		"hypnos hibernate deep-focus",
+		"hypnos hibernate --probe backup --script \"/usr/local/bin/backup.sh\" --duration 1h --recurrent",
+	},
 )
 
 var exampleScan = domovoi.FormatExample(
-	"hypnos",
-	[]string{"help"},
+	"hypnos scan",
+	[]string{
+		"hypnos scan",
+		"hypnos scan --verbose",
+	},
 )
 
-var examplePurge = domovoi.FormatExample(
-	"hypnos",
-	[]string{"help"},
+var exampleStasis = domovoi.FormatExample(
+	"hypnos stasis",
+	[]string{
+		"hypnos stasis focus",
+		"hypnos stasis --group deepwork",
+		"hypnos stasis --all",
+	},
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
